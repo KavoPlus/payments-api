9ce5df5c158470555603c484dabb070b3d38a972
Vinicius Araujo <vinicius.ifsp2018@gmail.com>
2026-01-21 21:03:15 +0000

    feat : complete payment logic with variables and statys display

--- app/index.js
+++ app/index.js
@@ -1,3 +1,19 @@
-console.log("Servidor de PAGAMENTOS Iniciado") ;
-console.log("Aguardando transacoes..");
-console.log ("Versao 1.0.0 - Ok");
+// Payment Data (Variables)
+const customerName = "Braia SRE";
+const product = "Cloud Course";
+const amount = 150.00    ;
+const currency = "USD" ;
+const isSucces = true;
+
+console.log("--- Payment Processing System ---");
+console.log(`Customer: ${customerName}`);
+console.log(`Purchased: ${product}`);
+console.log(`Amount: ${amount} ${currency}`);
+
+if (isSucces){ 
+    console.log ("Status : PAYMENT APPROVED🚀")
+ } else {
+    console.log("Status: PAYMENT FAILED! ❌");
+}
+
+console.log("---------------------------------");
