48e9ba016d30bd8b4b8ed92cd1b8ea14c1ed0e54
Vinicius Araujo <vinicius.ifsp2018@gmail.com>
2026-01-21 22:17:31 +0000

    feat:Using Docker

--- app/index.js
+++ app/index.js
@@ -1,16 +1,22 @@
 try {
-     console.log("--- Payment System Status: BOOTING ---");
+    console.log("--- Payment System Status: BOOTING ---");
 
-        const amount = 150.00;
-
-        console.log(`Processing payment of : ${amount} ${currency}`);
-}
-catch (error) {
-
-   console.log("[SRE ALERT] The system caught an error!");
-    console.log(`Error details: ${error.message}`);
+    // Usamos o Number() para garantir que o texto "200" vire o número 200
+const amount = Number(process.env.AMOUNT) || 0;
     
+    // Agora o código busca 'CURRENCY' do ambiente. 
+    // Se não encontrar, ele falha (o que vai disparar nosso Catch)
+    const currency = process.env.CURRENCY; 
+
+    if (!currency) {
+        throw new Error("Environment variable 'CURRENCY' is missing!");
+    }
+
+    console.log(`Processing payment of: ${amount} ${currency}`);
+
+} catch (error) {
+    console.log("[SRE ALERT] The system caught an error!");
+    console.log(`Error details: ${error.message}`);
 } finally {
     console.log("Cleaning up resources... System remains online.");
 }
-
